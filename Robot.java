e87cf806175edbd493e1f00457f5735c4fb2d726
Melanie McCord <56701478+haterofdietcoke@users.noreply.github.com>
2020-09-27 07:17:39 -0400

    Created ScientistRobot class

--- Robot.java
+++ Robot.java
@@ -2,7 +2,7 @@
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 /**
  * Robot Factory: Assignment 1
  * 
@@ -14,13 +14,31 @@ import java.util.List;
 public abstract class Robot {
 
     private String name;
-
     protected Robot(String name) {
         this.name = name;
     }
-    
+    protected String getName() {
+        return this.name;
+    }
     protected String speak() {
-        return "My name is " + name;
+        return "My name is " + getName();
     }
 
+}
+interface CitizenRobot {
+    public String showExpertise();
+}
+class ScientistRobot extends Robot implements CitizenRobot {
+    protected ScientistRobot(String name) {
+        super(name);
+    }
+    public String showExpertise() {
+        String temp = "";
+        temp += " and I am a scientist robot";
+        return temp;
+    }
+        @Override
+        protected String speak() {
+            return super.speak() + showExpertise();
+        }
 }
